51d7ec0a75afa91e81dfdf7a31e20f929fa79802
Dhruv Sharma <dhruvsharma0503@gmail.com>
2021-05-08 16:49:08 -0700

    Integrated Francescos work on login via OAuth.
    Added some login page elements purely for testing. Please
    remove those if you want to change that page

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -19,7 +19,7 @@ const useAuth = () => {
     return useContext(AuthContext)
 }
 
-const AuthProvider = ( { children } ) => {
+const AuthProvider = ({ children }) => {
 
     // TODO: Make this a null init. Right now this override helps testing
     const [currentUser, setCurrentUser] = useState('eruighfu')
@@ -27,9 +27,46 @@ const AuthProvider = ( { children } ) => {
     // Probably won't need this, except on the pages first load. For now it shouldn't mess anything up
     const [loading, setLoading] = useState(false)
 
+    const updateOauthStatus = (isSignedIn) => {
+        if (!isSignedIn) {
+            window.gapi.auth2.getAuthInstance().signIn()
+            return
+        }
+        setCurrentUser({
+            oauth: window.gapi.auth2.getAuthInstance().currentUser,
+            ...currentUser
+        })
+    }
+
     useEffect(() => {
-        gapiLoad()
-    }, [])
+        setLoading(true)
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                'apiKey': process.env.REACT_APP_GOOGLE_DRIVE_API_KEY,
+                'clientId': process.env.REACT_APP_GOOGLE_DRIVE_CLIENT_ID,
+                'scope': SCOPES,
+                'discoveryDocs': DISCOVERY_DOCS
+            }).then(() => {
+                window.gapi.auth2.getAuthInstance().isSignedIn.listen((isSignedIn) => {
+                    if (!isSignedIn) {
+                        window.gapi.auth2.getAuthInstance().signIn()
+                        return
+                    }
+                    setCurrentUser({
+                        oauth: window.gapi.auth2.getAuthInstance().currentUser,
+                        ...currentUser
+                    })
+                })
+                if (window.gapi.auth2.getAuthInstance().isSignedIn.get()) {
+                    setCurrentUser({
+                        oauth: window.gapi.auth2.getAuthInstance().currentUser,
+                        ...currentUser
+                    })
+                }
+            }, (e) => console.log(e.details))
+        })
+        setLoading(false)
+    }, [currentUser])
 
     const signup = () => {
         setLoading(true)
@@ -67,7 +104,7 @@ const AuthProvider = ( { children } ) => {
 
     const login = (user, pass) => {
         setLoading(true)
-        updateOauthStatus()
+        updateOauthStatus(window.gapi.auth2.getAuthInstance().isSignedIn.get())
         // validate actual pwd
         // setCurrentUser(---user obj---)
         // TODO
