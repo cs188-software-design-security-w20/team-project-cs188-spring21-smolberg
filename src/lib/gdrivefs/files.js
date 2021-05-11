a6637810990b4486f70e158b70a2f28b81acf70d
Shreya Raman <41533620+shreyaraman99@users.noreply.github.com>
2021-05-10 17:43:31 -0700

    added upload function

--- src/lib/gdrivefs/files.js
+++ src/lib/gdrivefs/files.js
@@ -20,4 +20,39 @@ const getAllFileData = (callback) => {
     getPage(window.gapi.client.drive.files.list(), [])
 }
 
-export { getAllFileData }
+const uploadFile = async () => {
+    const boundary='foo_bar_baz'
+    const delimiter = "\r\n--" + boundary + "\r\n";
+    const close_delim = "\r\n--" + boundary + "--";
+    var fileName='samplefile';
+    var fileData='sample data';
+    var contentType='text/plain'
+    var metadata = {
+        'name': fileName,
+        'mimeType': contentType
+    };
+
+    var multipartRequestBody =
+        delimiter +
+        'Content-Type: application/json; charset=UTF-8\r\n\r\n' +
+        JSON.stringify(metadata) +
+        delimiter +
+        'Content-Type: ' + contentType + '\r\n\r\n' +
+        fileData+'\r\n'+
+        close_delim;
+
+    console.log(multipartRequestBody);
+    var request = window.gapi.client.request({
+        'path': 'https://www.googleapis.com/upload/drive/v3/files',
+        'method': 'POST',
+        'params': {'uploadType': 'multipart'},
+        'headers': {
+            'Content-Type': 'multipart/related; boundary=' + boundary + ''
+        },
+        'body': multipartRequestBody});
+    request.execute(function(file) {
+        console.log(file)
+    });
+}
+
+export { getAllFileData, uploadFile }
