235616806b73ef17844312bf1cf53a26667b92e9
Dhruv Sharma <dhruvsharma0503@gmail.com>
2021-05-12 18:21:13 -0700

    Formatting fixes + temporary eslint disables

--- src/lib/manifest/filemanifest.js
+++ src/lib/manifest/filemanifest.js
@@ -1,50 +1,48 @@
-
+/* eslint-disable */
 
 /** File Manifest Constructor
- * 
+ *
  */
 const FileManifest = () => {
-	this.files = [];
-    //maybe add a manifest.settings or something
-    //if we do subdirectories, add a manifest.folder, which contains a similar data structure containing folders and files within
-	const addFile = (filename, encFilename, key, iv) => {
-		this.files[filename].key = key;
-		this.files[filename].iv = iv;
-		this.files[filename].encFilename;
-		
-		//save any other file metadata here
-	}
-	/** Gets file data needed to get and decrypt the file
-	 * 
-	 * @param {String} filename 
-	 * @returns {key, iv, encFilename}
-	 */
-	const getFile = (filename) => {
-		return this.files[filename]
-	}
-	/**
-	 * Deletes file from manifest
-	 * @param {String} filename File to delete 
-	 */
-	const deleteFile = (filename) => {
-		
-		delete this.files[filename];
-	}
-	/**
-	 * Renames a file in the manifest
-	 * @param {String} newFilename 
-	 * @param {String} oldFilename
-	 */
-	const renameFile = (newFilename, oldFilename) => {
-		
-		this.files[newFilename] = oldFilename;
-		delete this.files[oldFilename];
-	}
+  this.files = [];
+  //maybe add a manifest.settings or something
+  //if we do subdirectories, add a manifest.folder, which contains a similar data structure containing folders and files within
+  const addFile = (filename, encFilename, key, iv) => {
+    this.files[filename].key = key;
+    this.files[filename].iv = iv;
+    this.files[filename].encFilename;
 
-	const stringify = JSON.stringify(this);
-	const parse = (manifest) => { return JSON.parse(manifest); }
-}
+    //save any other file metadata here
+  };
+  /** Gets file data needed to get and decrypt the file
+   *
+   * @param {String} filename
+   * @returns {key, iv, encFilename}
+   */
+  const getFile = (filename) => {
+    return this.files[filename];
+  };
+  /**
+   * Deletes file from manifest
+   * @param {String} filename File to delete
+   */
+  const deleteFile = (filename) => {
+    delete this.files[filename];
+  };
+  /**
+   * Renames a file in the manifest
+   * @param {String} newFilename
+   * @param {String} oldFilename
+   */
+  const renameFile = (newFilename, oldFilename) => {
+    this.files[newFilename] = oldFilename;
+    delete this.files[oldFilename];
+  };
 
+  const stringify = JSON.stringify(this);
+  const parse = (manifest) => {
+    return JSON.parse(manifest);
+  };
+};
 
-
-export {FileManifest}
+export { FileManifest };
--- src/lib/manifest/usermanifest.js
+++ src/lib/manifest/usermanifest.js
@@ -1,18 +1,26 @@
+/* eslint-disable */
+
 const UserManifest = () => {
-    this.version = 0.1; //guess we are doing versioning here
-    this.fileManifestName = null;
-    this.fileManifestIV = null;
-    this.fileManifestKey = null;
+  this.version = 0.1; // guess we are doing versioning here
+  this.fileManifestName = null;
+  this.fileManifestIV = null;
+  this.fileManifestKey = null;
 
-    const setFileManifest = (fileManifestName, fileManifestIV, fileManifestKey) => {
-        this.fileManifestName = fileManifestName;
-        this.fileManifestIV = fileManifestIV;
-        this.fileManifestKey = fileManifestKey;
-    }
+  const setFileManifest = (
+    fileManifestName,
+    fileManifestIV,
+    fileManifestKey
+  ) => {
+    this.fileManifestName = fileManifestName;
+    this.fileManifestIV = fileManifestIV;
+    this.fileManifestKey = fileManifestKey;
+  };
 
-    const stringify = JSON.stringify(this);
-    const parse = (manifest) => { return JSON.parse(manifest); }
-    //we might want to prototype in upload and download functions for this
-}
+  const stringify = JSON.stringify(this);
+  const parse = (manifest) => {
+    return JSON.parse(manifest);
+  };
+  //we might want to prototype in upload and download functions for this
+};
 
-export {UserManifest}
+export { UserManifest };
